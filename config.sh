776e09e06817108462bf5a8d23f406c951d125d1
Bruce <111607498+Fanarrow@users.noreply.github.com>
2023-01-12 03:38:53 -0300

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+clear
+
+echo 'Selecione o que deseja executar.'
+
+# Lê as palavras expandidas, criando as opções do menu enumeradas.
+select OPCAO in 'Configurar Servidor' 'Instalar Aleatory Bot' 'Instalar Sakura Bot' 'Sair'
+do
+    # Lê o número armazenado na variável 'OPCAO' e executa a opção correspondente.
+    case $OPCAO in
+        'Configurar Servidor') # Opção 1
+            # Executando os comandos...
+            echo 'Configurando seu servidor.'
+rm -rf ReplitBot && git clone https://github.com/Fanarrow/ReplitBot.git && mv ReplitBot/* . && mv ReplitBot/.replit . && rm -rf ReplitBot
+            # Finaliza o menu
+            break
+            ;;
+        'Instalar Aleatory Bot') # Opção 2
+            # Executando os comandos...
+            echo 'Instalando Aleatory Bot.'
+sh aleatory.sh
+            # Finaliza o menu
+            break
+            ;;
+
+        'Instalar Sakura Bot')  # Opcao 3
+            # Executando os comandos...
+            echo 'Instalando Sakura Bot.'
+sh sakura.sh
+            # Finaliza o menu
+            break
+            ;;
+        'Sair')  # Finalizar script
+            echo 'Saindo...'
+            exit 0
+            ;;
+        *)  # Opção inválida. (nenhuma das opções anteriores).
+            # O comando break não é executado para que o fluxo do script
+            # permaneça preso ao menu, forçando o usuário a selecionar uma
+            # opção válida.
+            # Executando os comandos..
+            echo 'Opção seleciona é inválida.'
+            echo
+            ;;
+    esac
+done
+
+echo 'Terminado...'
